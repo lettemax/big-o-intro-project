159ab83ff003fd28ec65d5411ea98493281ea0a3
Max Lettenberger <maxlettenberger@gmail.com>
2020-11-18 13:57:43 -0800

    Debugging anagram

--- 02-anagrams.js
+++ 02-anagrams.js
@@ -1,6 +1,23 @@
 function firstAnagram(str1, str2) {
-  // Code goes here ....
+  let str2Arr = str2.split();
+  for (let i = 0; i < str1.length; i++) {
+    if (str2Arr.indexOf(str1[i]) > -1) {
+      let index = str2Arr.indexOf(str1[i]);
+      str2Arr.splice(index, 1);
+    } else {
+      return false;
+    }
+  }
+  if (str2Arr.length === 0) return true;
 }
+
+let str1 = 'stop'
+let str2 = 'tops'
+let str3 = 'dinner'
+
+console.log(firstAnagram(str1, str3));
+console.log(firstAnagram(str1, str2));
+
 
 
 
