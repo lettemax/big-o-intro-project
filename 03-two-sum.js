cb782d8bfb74c52ac843cd81d33eff837e156cf1
Max Lettenberger <maxlettenberger@gmail.com>
2020-11-18 15:21:34 -0800

    Okay twosum1

--- 03-two-sum.js
+++ 03-two-sum.js
@@ -1,10 +1,24 @@
 function badTwoSum(arr, targetSum) {
-  // Code goes here ...
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === targetSum) {
+        return true;
+      } 
+    }
+  }
+  return false;
 }
+
+console.log(badTwoSum([1, 5, 6, 9], 99));
+console.log(badTwoSum([1, 5, 6, 9], 14));
+
+
 
 
 function okayTwoSum1(arr, targetSum) {
-  // Code goes here ...
+  let sorted = arr.sort();
+  let pivot = Math.floor(arr.length / 2);
+  if (sorted.slice())
 }
 
 
