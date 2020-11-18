a79da1171ecdc9e2072247e22bd2c9850a5698a7
Quintin Hull <quintinhull92@gmail.com>
2020-11-18 18:36:51 -0500

    completed function okayTwoSum1

--- 03-two-sum.js
+++ 03-two-sum.js
@@ -3,34 +3,44 @@ function badTwoSum(arr, targetSum) {
     for (let j = i + 1; j < arr.length; j++) {
       if (arr[i] + arr[j] === targetSum) {
         return true;
-      } 
+      }
     }
   }
   return false;
 }
 
-console.log(badTwoSum([1, 5, 6, 9], 99));
-console.log(badTwoSum([1, 5, 6, 9], 14));
-
-
-
+// console.log(badTwoSum([1, 5, 6, 9], 99));
+// console.log(badTwoSum([1, 5, 6, 9], 14));
 
 function okayTwoSum1(arr, targetSum) {
+  if (arr.length <= 1) {
+    return false;
+  }
+
   let sorted = arr.sort();
   let pivot = Math.floor(arr.length / 2);
-  if (sorted.slice())
+  let sum = sorted[pivot] + sorted[pivot + 1];
+  if (sum < targetSum) {
+    let remaining = sorted.slice(pivot + 1);
+    return okayTwoSum1(remaining, targetSum);
+  } else if (sum > targetSum) {
+    let remaining = sorted.slice(0, pivot + 1);
+    return okayTwoSum1(remaining, targetSum);
+  } else {
+    return true;
+  }
 }
 
+console.log(okayTwoSum1([1, 5, 6, 9, 2, 6, 7], 91));
+console.log(okayTwoSum1([1, 5, 6, 9, 2, 6, 7], 97));
 
 function okayTwoSum2(arr, targetSum) {
   // Code goes here ...
 }
 
-
 function twoSum(arr, targetSum) {
   // Code goes here ...
 }
-
 
 function twoSumIndices(arr, targetSum) {
   // Code goes here ...
